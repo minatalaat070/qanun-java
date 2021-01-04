456eaebd6900651aa8fe7c062edd65649af42fd4
Mina Talaat <minatalaat007@gmail.com>
2021-01-04 13:18:35 +0200

    end of chapter 3 in java implementation

--- src/main/java/com/mina/qanun/Expr.java
+++ src/main/java/com/mina/qanun/Expr.java
@@ -4,12 +4,28 @@ import java.util.List;
 
 abstract class Expr {
 
+    interface Visitor<R> {
+
+        R visitBinaryExpr(Binary expr);
+
+        R visitGroupingExpr(Grouping expr);
+
+        R visitLiteralExpr(Literal expr);
+
+        R visitUnaryExpr(Unary expr);
+    }
+
     static class Binary extends Expr {
 
         Binary(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
             this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
         }
 
         final Expr left;
@@ -23,6 +39,11 @@ abstract class Expr {
             this.expression = expression;
         }
 
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+
         final Expr expression;
     }
 
@@ -30,6 +51,11 @@ abstract class Expr {
 
         Literal(Object value) {
             this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
         }
 
         final Object value;
@@ -42,7 +68,14 @@ abstract class Expr {
             this.right = right;
         }
 
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+
         final Token operator;
         final Expr right;
     }
+
+    abstract <R> R accept(Visitor<R> visitor);
 }
--- src/main/java/com/mina/tool/GenerateAst.java
+++ src/main/java/com/mina/tool/GenerateAst.java
@@ -40,11 +40,15 @@ public class GenerateAst {
             writer.println("import java.util.List;");
             writer.println();
             writer.println("abstract class " + baseName + " {");
+            defineVisitor(writer, baseName, exprTypes);
             for (String type : exprTypes) {
                 String className = type.split(":")[0].trim();
                 String fields = type.split(":")[1].trim();
                 defineType(writer, baseName, className, fields);
             }
+            // The base accept() method.
+            writer.println();
+            writer.println("  abstract <R> R accept(Visitor<R> visitor);");
             writer.println("}");
         }
     }
@@ -60,11 +64,30 @@ public class GenerateAst {
             writer.println("      this." + name + " = " + name + ";");
         }
         writer.println("    }");
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit"
+                + className + baseName + "(this);");
+        writer.println("    }");
 
         // Fields.
         writer.println();
         for (String field : fields) {
             writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
+
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> exprTypes) {
+        writer.println("  interface Visitor<R> {");
+        for (String type : exprTypes) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "("
+                    + typeName + " " + baseName.toLowerCase() + ");");
         }
 
         writer.println("  }");
