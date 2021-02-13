e0cf2aa47d1c4686324dc81b35faaa7757373951
Mina Talaat <minatalaat007@gmail.com>
2021-02-13 13:35:06 +0200

    removed unused imports, netbeans template comments lol

--- src/main/java/com/mina/qanun/Interpreter.java
+++ src/main/java/com/mina/qanun/Interpreter.java
@@ -1,18 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mina.qanun;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  *
